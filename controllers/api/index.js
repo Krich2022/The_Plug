ffb605fd177c050300100419b2f9c0dddea0e462
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2023-11-30 19:24:37 -0700

    Created api route files and changed index.js to point to the routes. I also created the post routes for the userRoutes file.

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const eventsRoutes = require("./eventsRoutes");
+const userEventsRoutes = requrie("./userEventsRoutes");
+
+module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.full_name,
+    });
+
+    req.session.save(() => {
+      req.session.logged_in = true;
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        [Op.or]: [{ email: req.body.email }, { username: req.body.username }],
+      },
+    });
+
+    if (!userData) {
+      res.status(400).json({ message: "Incorrect email or password" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect email or password" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
