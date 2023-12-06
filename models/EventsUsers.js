d0be11164756ff191e560e7aa3e6ae80371f3437
jjthedev <jjthedev94@gmail.com>
2023-12-05 21:24:53 -0700

    inserted sql object for event user

--- models/EventsUsers.js
+++ models/EventsUsers.js
@@ -3,15 +3,23 @@ const sequelize = require("../config/connection");
 
 class EventUser extends Model {}
 
-EventUser.init({
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+EventUser.init(
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  event_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "eventUser",
+  }
+);
 
 module.exports = EventUser;
