de880df7a3786b8b2209a087d7890243e71de30b
jjthedev <jjthedev94@gmail.com>
2023-12-05 20:51:51 -0700

    added api routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// CREATE new user
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
