2c12c90a9c12982376b9b04038ad96fcdc8dc07f
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2023-12-05 18:13:38 -0700

    worked on the javascript to pull the event table

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,33 +1,42 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('login', {
-            logged_in: req.session.logged_in 
-        })
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.render("login", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/signup', async (req, res) => {
-    try {
-        res.render('signup', {
-            logged_in: req.session.logged_in
-        })
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/dashboard', async (req, res) => {
-    try {
-        res.render('dashboard', {
-            logged_in: req.session.logged_in
-        })
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/dashboard", async (req, res) => {
+  try {
+    res.render("dashboard", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.get("/event/*", async (req, res) => {
+  try {
+    res.render("event", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
