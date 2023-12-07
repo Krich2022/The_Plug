0f984147a35bb5bd2f1042299929b09a716caaf8
Krich2020 <keston@therichfamily.net>
2023-12-07 11:38:52 -0700

    created function to handle sending the emails

--- controllers/api/emailRoutes.js
+++ controllers/api/emailRoutes.js
@@ -1,26 +1,35 @@
 const router = require("express").Router();
-const client = require("@sendgrid/mail");
-const { SENDGRID_API_KEY, SENDGRID_FROM_EMAIL } = process.env;
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const { SENDBLUE_API_KEY, SENDBLUE_FROM_EMAIL } = process.env;
 
 router.post("/", async (req, res) => {
   try {
     const { emails, eventName, eventStart, eventEnd, eventUrl } = req.body;
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    const apiKey = defaultClient.authentications["api-key"];
+    apiKey.apiKey = SENDBLUE_API_KEY;
 
-    const data = {
-      to: emails,
-      from: SENDGRID_FROM_EMAIL,
-      subject: `Event Reminder for ${eventName}`,
-      html: `This is a reminder for ${eventName}, the event begins at ${eventStart}`,
+    const apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
+    const emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+
+    emailCampaigns.name = "Campaign sent via the API";
+    emailCampaigns.subject = `RSVP Reminder for ${eventName}`;
+    emailCampaigns.sender = {
+      name: "The Plug",
+      email: SENDBLUE_FROM_EMAIL,
+    };
+    emailCampaigns.type = "classic";
+
+    emailCampaigns.htmlContent = `This is a reminder for ${eventName}, the event begins at ${eventStart} and ends at ${eventEnd}. If you have any questions, please visit the event page at ${eventUrl}<br><br>-The Plug`;
+
+    emailCampaigns.recipients = {
+      listIds: [],
+      emailAddresses: emails,
     };
 
-    if (eventEnd) {
-      data.html += ` and goes until ${eventEnd}`;
-    }
+    await apiInstance.createEmailCampaign(emailCampaigns);
 
-    data.html += `. If you have any questions, please visit the event page at ${eventUrl}<br><br>-The Plug`;
-
-    client.setApiKey(SENDGRID_API_KEY);
-    await client.send(data);
+    console.log("Email campaign created successfully");
 
     res.status(200).send("Message sent");
   } catch (err) {
