dba6672c68319e2de98d5254cc999386f16b1b1b
Devan Ash <141689897+DAsh365@users.noreply.github.com>
2023-12-07 01:01:53 -0700

    redo javascrpt

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,41 +1,44 @@
 const getAllEvent = async () => {
-    const event = await fetch(`/api/events/user-events`, {
+    const event = await fetch(/api/events/user-events, {
       method: "GET",
     });
-    const rsvp = await fetch(`/api/rsvp/rsvp-events`, {
+    const rsvp = await fetch(/api/rsvp/rsvp-events, {
       method: "GET",
     });
     const eventData = await event.json();
     const rsvpData = await rsvp.json();
     console.log(eventData);
     console.log(rsvpData);
-
-    for(i=0;i<rsvpData.length;i++){
-        const eventObj = rsvpData[i].event
-        const eventHtml = `<div
-        class="bg-gray-200 p-4 rounded cursor-pointer"
-        onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
-      >
-        <h2>${eventObj.event_name}</h2>
-        <p id="desc">${eventObj.event_desc}</p>
-        <p>Start:${eventObj.event_start} End:${eventObj.event_end}</p>
-        <p>Your RSVP event</p>
-      </div>`
-    document.getElementById("eventsContainer").innerHTML += eventHtml
+  
+    for (i = 0; i < rsvpData.length; i++) {
+      const eventObj = rsvpData[i].event;
+      const [endDate, timeEnd] = eventObj.event_end.split("T");
+      const [startDate, timeStart] = eventObj.event_start.split("T");
+      const eventHtml = <div
+          class="bg-gray-200 p-4 rounded cursor-pointer"
+          onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
+        >
+          <h2>${eventObj.event_name}</h2>
+          <p id="desc">${eventObj.event_desc}</p>
+          <p>Start: ${startDate} End: ${endDate}</p>
+          <p>Your RSVP event</p>
+        </div>;
+      document.getElementById("eventsContainer").innerHTML += eventHtml;
     }
-
-    for(i=0;i<eventData.length;i++){
-        const eventObj = eventData[i]
-        const eventHtml = `<div
-        class="bg-gray-200 p-4 rounded cursor-pointer"
-        onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
-      >
-        <h2>${eventObj.event_name}</h2>
-        <p id="desc">${eventObj.event_desc}</p>
-        <p>Start:${eventObj.event_start} End:${eventObj.event_end}</p>
-        <p>Your event</p>
-      </div>`
-    document.getElementById("eventsContainer").innerHTML += eventHtml
-}};
-
-getAllEvent
+  
+    for (i = 0; i < eventData.length; i++) {
+      const eventObj = eventData[i];
+      const eventHtml = <div
+          class="bg-gray-200 p-4 rounded cursor-pointer"
+          onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
+        >
+          <h2>${eventObj.event_name}</h2>
+          <p id="desc">${eventObj.event_desc}</p>
+          <p>Start:${startDate} End:${endDate}</p>
+          <p>Your event</p>
+        </div>;
+      document.getElementById("eventsContainer").innerHTML += eventHtml;
+    }
+  };
+  
+  getAllEvent();
