648835bf7961d3b4c4bccfcaed1652ab609c8405
Devan Ash <141689897+DAsh365@users.noreply.github.com>
2023-12-07 01:47:06 -0700

    redo javascrpt2

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,8 @@
 const getAllEvent = async () => {
-    const event = await fetch(/api/events/user-events, {
+    const event = await fetch("/api/events/user-events", {
       method: "GET",
     });
-    const rsvp = await fetch(/api/rsvp/rsvp-events, {
+    const rsvp = await fetch("/api/rsvp/rsvp-events", {
       method: "GET",
     });
     const eventData = await event.json();
@@ -14,29 +14,29 @@ const getAllEvent = async () => {
       const eventObj = rsvpData[i].event;
       const [endDate, timeEnd] = eventObj.event_end.split("T");
       const [startDate, timeStart] = eventObj.event_start.split("T");
-      const eventHtml = <div
+      const eventHtml = `<div
           class="bg-gray-200 p-4 rounded cursor-pointer"
-          onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
+          onclick="showEvent(${eventData.id})"
         >
           <h2>${eventObj.event_name}</h2>
           <p id="desc">${eventObj.event_desc}</p>
           <p>Start: ${startDate} End: ${endDate}</p>
           <p>Your RSVP event</p>
-        </div>;
+        </div>`
       document.getElementById("eventsContainer").innerHTML += eventHtml;
     }
   
     for (i = 0; i < eventData.length; i++) {
       const eventObj = eventData[i];
-      const eventHtml = <div
+      const eventHtml = `<div
           class="bg-gray-200 p-4 rounded cursor-pointer"
-          onclick="viewEvent('Event 1', 'January 1, 2023, 12:00 PM', 'Event created by: John Doe')"
+          onclick="showEvent(${eventData.id})"
         >
           <h2>${eventObj.event_name}</h2>
           <p id="desc">${eventObj.event_desc}</p>
           <p>Start:${startDate} End:${endDate}</p>
           <p>Your event</p>
-        </div>;
+        </div>`
       document.getElementById("eventsContainer").innerHTML += eventHtml;
     }
   };
